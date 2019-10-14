a60555ad5241211f6299b22d583857e66c363733
cabreradaniel4 <daniel_cabrera99@outlook.com>
2019-10-14 10:30:29 -0500

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,14 @@
+
+username = ""
+def saludarUser(username='daniel'):
+	username = input("Ingresa tu username :")
+	print("Hola "+username)
+
+	
+
+saludarUser(username)
+def suma(num1=0,num2=0):
+	total=num1+num2
+	print(total)
+
+	suma(num1,)
